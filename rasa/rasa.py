6f8bb6f4c4fe8599eb3f7cf7a7e611899c184e18
ogenstad <patrick@ogenstad.com>
2015-05-18 22:38:56 +0200

    Options and object functions

--- rasa/rasa.py
+++ rasa/rasa.py
@@ -10,37 +10,45 @@ HEADERS = {
     'User-Agent': 'RASA'
 }
 
-
-
 class ASA(object):
 
-
-    def __init__(self, device, username, password):
-
+    def __init__(self, device=None, username=None, password=None, verify_cert=True, timeout=5):
+        
         self.device = device
         self.username = username
         self.password = password
-
+        self.verify_cert = verify_cert
+        self.timeout = timeout
         self.cred = HTTPBasicAuth(self.username, self.password)
+
+    def _delete(self, request):
+        url = 'https://' + self.device + '/api/' + request
+        data = requests.delete(url,headers=HEADERS,auth=self.cred, verify=self.verify_cert, timeout=self.timeout)
+        return data
 
     def _get(self, request):
         url = 'https://' + self.device + '/api/' + request
-        data = requests.get(url,headers=HEADERS,auth=self.cred, verify=False)
-        #return data
-        if data.status_code == 200:
-            return data.json()
-        else:
-            return {}        
+        data = requests.get(url,headers=HEADERS,auth=self.cred, verify=self.verify_cert, timeout=self.timeout)
+        return data
 
     def _patch(self, request, data):
         url = 'https://' + self.device + '/api/' + request
-        data = requests.patch(url, data=json.dumps(data), headers=HEADERS, auth=self.cred, verify=False)
+        data = requests.patch(url, data=json.dumps(data), headers=HEADERS, auth=self.cred, verify=self.verify_cert, timeout=self.timeout)
         return data
 
-    def _post(self, request, data):
+    def _post(self, request, data=False):
         url = 'https://' + self.device + '/api/' + request
-        data = requests.post(url, data=json.dumps(data), headers=HEADERS, auth=self.cred, verify=False)
+        if data != False:
+            data = requests.post(url, data=json.dumps(data), headers=HEADERS, auth=self.cred, verify=self.verify_cert, timeout=self.timeout)
+        else:
+            data = requests.post(url, headers=HEADERS, auth=self.cred, verify=self.verify_cert, timeout=self.timeout)            
         return data
+
+    def _put(self, request, data):
+        url = 'https://' + self.device + '/api/' + request
+        data = requests.put(url, data=json.dumps(data), headers=HEADERS, auth=self.cred, verify=self.verify_cert, timeout=self.timeout)
+        return data
+
 
     def get_access_in(self):
         request = 'access/in'
@@ -58,6 +66,29 @@ class ASA(object):
         request = 'objects/localusers'
         return self._get(request)
 
+    # Network objects
+
+    def delete_networkobject(self, net_object):
+        request = 'objects/networkobjects/' + net_object
+        return self._delete(request)
+
+    def get_networkobject(self, net_object):
+        request = 'objects/networkobjects/' + net_object
+        return self._get(request)
+
+    def get_networkobjects(self):
+        request = 'objects/networkobjects'
+        return self._get(request)
+
+    def replace_networkobject(self, name, data):
+        request = 'objects/networkobjects/' + name
+        return self._put(request, data)
+
+    def set_networkobject(self, data):
+        request = 'objects/networkobjects'
+        return self._post(request, data)
+
+    # Network object-groups
     def get_networkobjectgroups(self):
         request = 'objects/networkobjectgroups'
         return self._get(request)
@@ -69,4 +100,8 @@ class ASA(object):
     def replace_networkobjectgroup(self, group, data):
         request = 'objects/networkobjectgroups/' + group
         return self._patch(request, data)
+
+    def write_mem(self):
+        request = '/api/commands/writemem'
+        return self._post(request)
 
